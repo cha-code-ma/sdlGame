79772127edae4b3ae939edf55c5176e92a638753
Chakir <chakir.el.mallali@student.uva.nl>
2026-07-30 12:20:48 +0200

    upd

--- gameFiles/physics.c
+++ gameFiles/physics.c
@@ -15,6 +15,14 @@ bool collision(object* obj1, object* obj2) {
 	if (!obj1 || !obj2 ||!obj1->obj_layer || !obj2->obj_layer) {
 		return false;
 	}
+	vec2 lu1;
+	vec2 ru1;
+	vec2 ll1;
+	vec2 rl1;
+	vec2 lu2;
+	vec2 ru2;
+	vec2 ll2;
+	vec2 rl2;
 	if (obj1->obj_shape == SQUARE) {
 		vec2 lu1 = {obj1->future_position.x, obj1->future_position.y }; //lu: left upper
 		vec2 ru1 = {obj1->future_position.x + obj1->future_size.x, obj1->future_position.y }; //ru: right upper
@@ -28,7 +36,10 @@ bool collision(object* obj1, object* obj2) {
 		vec2 rl2 = {obj2->future_position.x + obj2->future_size.x, obj2->future_position.y + obj2->future_size.y }; //lu: right lower
 	}
 	if (obj1->obj_shape == SQUARE && obj2->obj_shape == SQUARE) {
-		int total = 0
+		int total = 0;
+		if (lu1.x < lu2.x && ll1.x < ll2.x &&   ) {
+
+		}
 	}
 }
 
