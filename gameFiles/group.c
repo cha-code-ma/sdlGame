5170555e853cd1f735ddddbe301e777df5d45425
Chakir <chakir.el.mallali@student.uva.nl>
2026-08-13 13:11:48 +0200

    adding groups, so u can do an action on a group of objects/ UI elements, this is used by for example buttons

--- gameFiles/group.c
+++ gameFiles/group.c
@@ -44,3 +44,85 @@ void group_action_hide(group_t *group, bool with_objects, bool with_UI) {
         }
     }
 }
+
+void group_action_show(group_t *group, bool with_objects, bool with_UI) {
+    if (!group || !group->objects) {
+        return;
+    }
+    int count;
+    if (with_objects) {
+        object **objects = object_pool_get_objects(group->objects, &count);
+        if (!objects) {
+            return;
+        }
+        for (int i = 0; i < count; i++) {
+            int sub_count;
+            sub_object **sub_objects = sub_object_pool_get_objects(objects[i]->sub_objects, &sub_count);
+            for (int j = 0; j < sub_count; j++) {
+                sub_objects[j]->visible = true;
+            }
+        }
+    }
+    if (with_UI) {
+        int ui_count;
+        object **ui_objects = object_pool_get_objects(group->ui->ui_objects, &ui_count);
+        if (!ui_objects) {
+            return;
+        }
+        for (int i = 0; i < ui_count; i++) {
+            int sub_count;
+            sub_object **sub_objects = sub_object_pool_get_objects(ui_objects[i]->sub_objects, &sub_count);
+            for (int j = 0; j < sub_count; j++) {
+                sub_objects[j]->visible = true;
+            }
+        }
+        button **buttons = button_pool_get_objects(group->ui->buttons, &ui_count);
+        if (!buttons) {
+            return;
+        }
+        for (int i = 0; i < ui_count; i ++) {
+            buttons[i]->visible = true;
+        }
+    }
+}
+
+void group_action_invert_visibility(group_t *group, bool with_objects, bool with_UI) {
+    if (!group || !group->objects) {
+        return;
+    }
+    int count;
+    if (with_objects) {
+        object **objects = object_pool_get_objects(group->objects, &count);
+        if (!objects) {
+            return;
+        }
+        for (int i = 0; i < count; i++) {
+            int sub_count;
+            sub_object **sub_objects = sub_object_pool_get_objects(objects[i]->sub_objects, &sub_count);
+            for (int j = 0; j < sub_count; j++) {
+                sub_objects[j]->visible = !sub_objects[j]->visible;
+            }
+        }
+    }
+    if (with_UI) {
+        int ui_count;
+        object **ui_objects = object_pool_get_objects(group->ui->ui_objects, &ui_count);
+        if (!ui_objects) {
+            return;
+        }
+        for (int i = 0; i < ui_count; i++) {
+            int sub_count;
+            sub_object **sub_objects = sub_object_pool_get_objects(ui_objects[i]->sub_objects, &sub_count);
+            for (int j = 0; j < sub_count; j++) {
+                sub_objects[j]->visible = !sub_objects[j]->visible ;
+            }
+        }
+        button **buttons = button_pool_get_objects(group->ui->buttons, &ui_count);
+        if (!buttons) {
+            return;
+        }
+        for (int i = 0; i < ui_count; i ++) {
+            buttons[i]->visible = !buttons[i]->visible;
+        }
+    }
+}
